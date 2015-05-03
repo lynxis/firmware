857a5082eb9ea37ef0d634e6b7000ed3e899fef0
Alexander Couzens <lynxis@fe80.eu>
2015-05-03 17:31:29 +0200

    config.mk: update openwrt trunk to ea55d56b2

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PACKAGES_LIST_DEFAULT=default
 OPENWRT_SRC=git://git.openwrt.org/openwrt.git
-OPENWRT_COMMIT=0bf55fc27a3faea59454ed8f0ab15b82d1d7b88e
+OPENWRT_COMMIT=ea55d56b2f0bff3ce60f0b09b352e3ccab850c9f
 MAKE_ARGS=
